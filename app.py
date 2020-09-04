14779a45cb98bd2fb9d189b93c10841734df402f
Aaron Renfroe <aaronrenfroe@Aarons-MacBook-Pro-2.local>
2020-09-03 22:34:21 -0400

    fixed route bug

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import config
 import model
 
 from flask import Flask
@@ -5,10 +6,10 @@ app = Flask(__name__)
 
 app.add_url_rule('/', 'home', model.home)
 app.add_url_rule('/hello-flask','hello_flask', model.hello_flask)
-app.add_url_rule('/test','hello_flask', model.home)
+app.add_url_rule('/test','hello_flask2', model.home)
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0', port=config.PORT)
 
 # random comment
 # Faster Java deployments
